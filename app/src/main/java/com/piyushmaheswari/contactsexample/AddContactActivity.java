331ad0ec6908a553f70c7f4f4dde89c9e6c64092
Piyush2769 <startcheckingyourmail@gmail.com>
2019-09-10 12:25:05 +0530

    Initial Commit

---
+++ app/src/main/java/com/piyushmaheswari/contactsexample/AddContactActivity.java
@@ -0,0 +1,20 @@
+package com.piyushmaheswari.contactsexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class AddContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    public void okButtonClicked(View view) {
+    }
+
+    public void cancelButtonClicked(View view) {
+    }
+}
