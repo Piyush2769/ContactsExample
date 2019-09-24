be16bce3db68489ce84704a7beabec343490935e
Piyush2769 <startcheckingyourmail@gmail.com>
2019-09-24 21:14:53 +0530

    Initial Commit

--- app/src/main/java/com/piyushmaheswari/contactsexample/MainActivity.java
+++ app/src/main/java/com/piyushmaheswari/contactsexample/MainActivity.java
@@ -2,8 +2,20 @@ package com.piyushmaheswari.contactsexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.SimpleAdapter;
+import android.widget.SimpleCursorAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private SimpleCursorAdapter simpleCursorAdapter;
+
+    private RelativeLayout relativeLayout;
+    private LinearLayout linearLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
